a838558c9a4379978280da13a1b711b600efca49
Ryan Keller <rykeller@uw.edu>
2017-08-08 14:42:13 -0700

    Switched over to src_users example from minimal-reduxx

--- _src/router.js
+++ _src/router.js
@@ -3,7 +3,6 @@ import { Router, Route, IndexRoute } from "react-router";
 import { history } from "./store.js";
 import App from "./components/App";
 import Home from "./components/Home";
-import UserEdit from "./components/UserEdit";
 import NotFound from "./components/NotFound";
 
 // build the router
@@ -11,7 +10,6 @@ const router = (
   <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
-      <Route path="user-edit(/:id)" component={UserEdit}/>
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
