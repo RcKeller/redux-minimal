7e549d5636f32d78758aa38a903c7a70428ea106
Ryan Keller <rykeller@uw.edu>
2017-08-08 15:33:16 -0700

    Frustrating, hot reloading is not targetting the right dirs.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="page-home">
-        TESTFDSSDFSFD
+        TEST2fdsdf
         <UserList/>
       </div>
     );
