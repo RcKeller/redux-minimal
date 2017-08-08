e5054ec6e48075f2b5acd3479da3e097e08b6469
Ryan Keller <rykeller@uw.edu>
2017-08-08 15:01:20 -0700

    Broken, but found how to specify options for webpack config location.

---
+++ webpack/webpack.config.js
@@ -0,0 +1,61 @@
+// http://webpack.github.io/docs/configuration.html
+// http://webpack.github.io/docs/webpack-dev-server.html
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+const ENV = process.env.NODE_ENV === 'production' || 'development'
+module.exports = {
+  entry: [
+    // http://gaearon.github.io/react-hot-loader/getstarted/
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    __dirname + '/src/index.js',
+  ],
+  output: {
+    path: __dirname + '/public/js',
+    publicPath: 'js/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'react-hot-loader'
+        }, {
+          loader: 'babel-loader'
+        }],
+        exclude: /node_modules/,
+      },
+      {
+        // https://github.com/jtangelder/sass-loader
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }],
+      }
+    ],
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+  },
+  plugins: [
+    new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
+      root: __dirname + '/public',
+      verbose: true,
+      dry: false, // true for simulation
+    }),
+  ],
+};
