ec368e67f3d60b437b1a640b337ed761ae1df163
Ryan Keller <rykeller@uw.edu>
2017-08-12 16:17:38 -0700

    Working with ONE config file. Needs refactoring.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,10 @@
 // http://webpack.github.io/docs/configuration.html
 // http://webpack.github.io/docs/webpack-dev-server.html
+var webpack = require('webpack')
 var path = require('path')
 var CleanWebpackPlugin = require('clean-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const ENV = process.env.NODE_ENV
-console.warn()
 const CURRENT_WORKING_DIR = process.cwd()
 const PATHS = {
   app: path.resolve(CURRENT_WORKING_DIR, 'src'),
@@ -12,127 +12,112 @@ const PATHS = {
   public: path.resolve(CURRENT_WORKING_DIR, 'public'),
   bundle: path.resolve(CURRENT_WORKING_DIR, 'public', 'js'),
   modules: path.resolve(CURRENT_WORKING_DIR, 'node_modules')
-  // antd: path.resolve(CURRENT_WORKING_DIR, 'node_modules/antd')
 }
 
 module.exports = (env = {}) => {
-  const core = {
-    entry: [
-      // http://gaearon.github.io/react-hot-loader/getstarted/
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/only-dev-server',
-      'babel-polyfill',
-      PATHS.entry
-    ],
-    output: {
-      path: PATHS.bundle,
-      publicPath: 'js/',
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-          test: /\.js$/,
-          use: [
-            { loader: 'react-hot-loader' },
-            { loader: 'babel-loader' }
-          ],
-          exclude: /node_modules/
-        }, {
-          test: /\.scss$/,
-          use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' },
-            {
-              loader: 'sass-loader',
-              // options: {
-              //     sourceMap: true,
-              //     includePaths: [
-              //         path.resolve(process.cwd(), 'node_modules'),
-              //     ]
-              // }
-            }
-          ],
-          include: [PATHS.app, PATHS.modules]
-        }, {
-          test: /\.css$/,
-          use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' }
-          ]
-        }]
-    },
-    devServer: { contentBase: PATHS.public },
-    plugins: [
+  //  https://blog.flennik.com/the-fine-art-of-the-webpack-2-config-dc4d19d7f172
+  const isProduction = env.production === true
+  console.warn('PRODUCTION', isProduction)
+
+  //  Route entry point to webpack-dev-server in development
+  const entry = () => {
+    return isProduction
+      ? [PATHS.entry]
+      : [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        PATHS.entry
+      ]
+  }
+
+  //  Add hot-reloading and quality-of-life babel presets/plugins in development
+  const rules = () => {
+    let babel = {
+      presets: ['es2015', 'react', 'stage-0'],
+      plugins: ['transform-decorators-legacy']
+    }
+    if (!isProduction) {
+      babel.presets = ['react-hmre', ...babel.presets]
+      babel.plugins.push([
+        'transform-react-remove-prop-types',
+        'transform-react-constant-elements',
+        'transform-react-inline-elements'
+      ])
+    }
+    return [{
+      test: /\.js$|\.jsx$/,
+      use: [
+        { loader: 'react-hot-loader' },
+        {
+          loader: 'babel-loader',
+          options: babel
+        }
+      ],
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            includePaths: [PATHS.modules]
+          }
+        }
+      ],
+      include: [PATHS.app, PATHS.modules]
+    }, {
+      test: /\.css$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' }
+      ]
+    }]
+
+  }
+
+  const plugins = () => {
+    //  Always clean out prior bundles
+    let corePlugins = [
       new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
         root: PATHS.public,
         verbose: true,
         dry: false // true for simulation
       })
     ]
-  }
-  const prod = core
-  prod.entry =  [
-    'babel-polyfill',
-    process.cwd() + '/src/index.js'
-  ]
-
-
-  const config = ''
-  return config
-
-}
-
-module.exports = {
-  entry: [
-    // http://gaearon.github.io/react-hot-loader/getstarted/
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    'babel-polyfill',
-    PATHS.entry
-  ],
-  output: {
-    path: PATHS.bundle,
-    publicPath: 'js/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        use: [
-          { loader: 'react-hot-loader' },
-          { loader: 'babel-loader' }
-        ],
-        exclude: /node_modules/
-      }, {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-                sourceMap: true,
-                includePaths: [
-                    path.resolve(process.cwd(), 'node_modules'),
-                ]
-            }
+    //  Set env and uglify/extract text in production builds
+    if (isProduction) {
+      corePlugins.push(
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production')
           }
-        ],
-        include: [PATHS.app, PATHS.modules]
-      }, {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' }
-        ]
-      }]
-  },
-  devServer: { contentBase: PATHS.public },
-  plugins: [
-    new CleanWebpackPlugin(['css/main.css', 'js/bundle.js'], {
-      root: PATHS.public,
-      verbose: true,
-      dry: false // true for simulation
-    })
-  ]
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          comments: false,
+          compressor: {
+            warnings: false
+          }
+        }),
+        new ExtractTextPlugin('../css/main.css')
+      )
+    }
+    return corePlugins
+  }
+
+  return {
+    entry: entry(),
+    output: {
+      path: PATHS.bundle,
+      publicPath: 'js/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: rules()
+    },
+    devServer: { contentBase: PATHS.public },
+    plugins: plugins()
+  }
 }
